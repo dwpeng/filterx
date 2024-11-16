472271e2adf42e48292b87720b0af9ed85dda0c3
dwpeng <dwpeng1122@foxmail.com>
2024-11-16 15:30:54 +0800

    fix: parse multi-member gzip files correctly

--- src/filterx_source/src/block/reader.rs
+++ src/filterx_source/src/block/reader.rs
@@ -1,4 +1,4 @@
-use flate2::read::GzDecoder;
+use flate2::read::MultiGzDecoder as GzDecoder;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Seek, SeekFrom};
 use std::ops::{Deref, DerefMut};
